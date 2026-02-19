ad83939a6e13c5cb3fdd37a408eacbeac58708f6
chin <developer_02@jeweal.co.th>
2026-02-19 10:38:54 +0700

    JWT_EXPIRE

--- src/utils/generateAccessToken.js
+++ src/utils/generateAccessToken.js
@@ -1,9 +1,9 @@
 const jwt = require("jsonwebtoken");
-
+const PORT = process.env.JWT_EXPIRE || "1D";
 module.exports = (user) => {
   return jwt.sign(
     { id: user._id, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: "15m" }
+    { expiresIn: PORT}
   );
 };
